e92c5bc73ee57c4e3d6fbc98578d18e604311e86
Kevin Phair <phair.kevin@gmail.com>
2016-07-14 23:38:55 -0400

    fix some things, tidy up, use the things

--- lib/App/showreverse.pm
+++ lib/App/showreverse.pm
@@ -6,16 +6,17 @@ package App::showreverse;
 
 use base qw(App::Cmd::Simple);
 
+use Net::CIDR 0.18 qw/cidr2octets cidrvalidate/;
+use Net::DNS 1.01;
 use Net::Works::Network 0.21;
- 
- 
+
 sub validate_args {
-    my ($self, $opt, $args) = @_;
- 
+    my ( $self, $opt, $args ) = @_;
+
     unless ( @$args >= 1 ) {
         $self->usage_error('Try #sr showreverse <space separated cidr blocks>');
     }
-    my @blocks = @{ $args };
+    my @blocks = @{$args};
     for my $block (@blocks) {
         unless ( cidrvalidate($block) ) {
             $self->usage_error("$block is not a valid cidr block");
@@ -23,24 +24,25 @@ sub validate_args {
     }
     return 1;
 }
-      
+
 sub execute {
     my ( $self, $opt, $args ) = @_;
-    my @blocks = @{ $args };
+    my @blocks = @{$args};
 
     my $resolver = Net::DNS::Resolver->new;
 
     for my $block (@blocks) {
-        my $n = Net::Works::Network->($block);
-        while ( my $ip = $n->next ) {
+        my $n = Net::Works::Network->new_from_string(string => $block);
+        my $i = $n->iterator;
+        while ( my $ip = $i->() ) {
             my $reverse = join( '.', reverse( split /\./, $ip ) ) . '.in-addr.arpa';
             if ( my $ap = $resolver->query( $reverse, 'PTR' ) ) {
                 for my $pa ( $ap->answer ) {
-                    print "$address => ", $pa->ptrdname, $/;
+                    print "$ip => ", $pa->ptrdname, $/;
                 }
             }
             else {
-                print "$address => NXDOMAIN\n";
+                print "$ip => NXDOMAIN\n";
             }
         }
     }
